17928d4a156f882f533e240088968addb379439f
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-12-18 11:19:57 -0700

    Refactoring again.

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,7 @@
 
 namespace src;
 
-class Program
-{
+class Program {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
